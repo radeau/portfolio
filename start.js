bc63abd4747270835d8fa619ced2ef4c6cc0a4d2
radeau <rad_kali@Rad.>
2024-02-02 23:27:06 +0400

    Update content

--- start.js
+++ start.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
 		
-    el_autohide = document.querySelector('.autohide');
+    autohide = document.querySelector('.autohide');
     
     // add padding-top to bady (if necessary)
     navbar_height = document.querySelector('.navbar').offsetHeight;
     document.body.style.paddingTop = navbar_height + 'px';
 
-    if(el_autohide){
+    if(autohide){
         
         var last_scroll_top = 0;
         window.addEventListener('scroll', function() {
                let scroll_top = window.scrollY;
            if(scroll_top < last_scroll_top) {
-                el_autohide.classList.remove('scrolled-down');
-                el_autohide.classList.add('scrolled-up');
+                autohide.classList.remove('scrolled-down');
+                autohide.classList.add('scrolled-up');
             }
             else {
-                el_autohide.classList.remove('scrolled-up');
-                el_autohide.classList.add('scrolled-down');
+                autohide.classList.remove('scrolled-up');
+                autohide.classList.add('scrolled-down');
             }
             last_scroll_top = scroll_top;
 
